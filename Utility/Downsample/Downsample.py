f6325e85d14b687ebd87f60b4951d41b92f2ef8d
Vesa Vahermaa <schwerbelastung@Vesas-MacBook-Pro-2.local>
2018-11-23 22:58:49 +0200

    Combined downsample and csv-write script

--- Utility/Downsample/Downsample.py
+++ Utility/Downsample/Downsample.py
@@ -9,52 +9,60 @@ Created on Fri Nov 23 21:11:47 2018
 # Import required libraries
 
 import scipy.io.wavfile
+import argparse
+import numpy
 
 # Load data
 
-file = scipy.io.wavfile.read("/Users/schwerbelastung/Desktop/Junction/DataLocation/IHearVoicesData/Audio/device1_channel1_20181012124210.wav", mmap=False)
+# "/Users/schwerbelastung/Desktop/Junction/DataLocation/IHearVoicesData/Audio/device1_channel1_20181012124210.wav"
+
+parser = argparse.ArgumentParser("Downsample")
+parser.add_argument("PathToWav", help="Full path to WAV file, CSV will be output as 'csv' or 'npy' with the same name")
+parser.add_argument("TargetType", help="Target file type 'csv' (textual csv - slower) or 'npy' (binary numpy - faster)")
+parser.add_argument("DownsamplingRate", help="Enter how much you want to downsample. 1 = no downsampling, 2 = 1/2 of samples, n = 1/n of samples")
+parser.add_argument("Minutes", help="Enter how much you want to downsample. 1 = no downsampling, 2 = 1/2 of samples, n = 1/n of samples.")
+args = parser.parse_args()
+
+file = scipy.io.wavfile.read(args.PathToWav)
+downsample_rate = int(args.DownsamplingRate)
+mins_to_isolate = int(args.Minutes)
+
+
+
+
+# Get sample rate
+fileLength = len(file[1])
+samplingRate = file[0]
+samplesInMinute = samplingRate*60
 
 # Calculate data file length
 # The 1st array entry has the sampling rate, the 2nd has an array of sound
 # values over time.
 
-length_in_seconds = (len(file[1]) / file[0])
+length_in_seconds = (fileLength / samplingRate)
 length_in_minutes = length_in_seconds/60
 
 print("The audiofile length in seconds: ", length_in_seconds)
 print("The audiofile length in minutes: ", length_in_minutes)
 
-# Extract last 1 / 3 / 5 / 10 minutes.
-# Logic: Sample rate tells us number of measurements in 1 second, multiply by 60
+# Extract last n minutes.
 
+startpoint = fileLength - mins_to_isolate
 
-minute =  file[0]*60
-startpoint_1min = len(file[1]) - 1*minute
-startpoint_3min = len(file[1]) - 3*minute
-startpoint_5min = len(file[1]) - 5*minute
-startpoint_10min = len(file[1]) - 10*minute
+sliceObj = slice(startpoint,fileLength)
 
-last_1min_sliceObj = slice(startpoint_1min,len(file[1]))
-last_3min_sliceObj = slice(startpoint_3min,len(file[1]))
-last_5min_sliceObj = slice(startpoint_5min,len(file[1]))
-last_10min_sliceObj = slice(startpoint_10min,len(file[1]))
+lastpart = file[1][sliceObj]
 
-last_1min = file[1][last_1min_sliceObj]
-last_3min = file[1][last_3min_sliceObj]
-last_5min = file[1][last_5min_sliceObj]
-last_10min = file[1][last_10min_sliceObj]
+# Downsample to reduce file size
 
-# Delete unnecessary variables
-
-del (startpoint_1min,startpoint_3min,startpoint_5min,startpoint_10min)
-
-# Downsample to different sizes, to reduce data size
-
-file_downsampled_2 = file[1][::2]
-file_downsampled_4 = file[1][::4]
-file_downsampled_8 = file[1][::8]
-file_downsampled_16 = file[1][::16]
-file_downsampled_32 = file[1][::32]
+downsampled_file = file[1][::downsample_rate]
 
 
 # Extract to csv
+
+if (args.TargetType == 'csv'):
+	numpy.savetxt(args.PathToWav+'.csv', downsampled_file, delimiter=",")
+elif (args.TargetType == 'npy'):
+	numpy.save(args.PathToWav+'.npy', downsampled_file)
+else:
+	parser.print_help()
